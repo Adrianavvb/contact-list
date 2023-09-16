1909f156ed21e994f59128f8b3e6a727b49f7637
Adrianavvb <adrianavvb29@gmail.com>
2023-09-16 16:06:15 -0400

    cambios en js

--- lista.js
+++ lista.js
@@ -1,31 +1,115 @@
 //Lista de contactos
-const listaContactos = ["Adriana Villarreal", "Daniel Reyes", "Benjamin Reyes"];
+let listaContactos = [
+  {
+    id: 1,
+    nombre: "Adriana",
+    apellido: "Villarreal",
+    telefono: "020305060",
+    ubicaciones: {
+      ciudad: "Venezuela",
+      direccion: "Avenidad principal 395",
+    },
+  },
+
+  {
+    id: 2,
+    nombre: "Daniel",
+    apellido: "Reyes",
+    telefono: "020305060",
+    ubicaciones: {
+      ciudad: "Venezuela",
+      direccion: "Avenidad principal 390",
+    },
+  },
+
+  {
+    id: 3,
+    nombre: "Benjamin",
+    apellido: "Reyes",
+    telefono: "024505060",
+    ubicaciones: {
+      ciudad: "Venezuela",
+      direccion: "Avenidad principal 350",
+    },
+  },
+];
 
 //Añadir contacto
 
 function agregarContactos(contactos) {
   listaContactos.push(contactos);
 }
-agregarContactos("Jackeline Barboza");
-agregarContactos("Rafael Villarreal");
+agregarContactos({
+  id: 4,
+  nombre: "Jackeline",
+  apellido: "Barboza",
+  telefono: "020334560",
+  ubicaciones: {
+    ciudad: "Venezuela",
+    direccion: "Avenidad principal 342",
+  },
+});
+agregarContactos({
+  id: 5,
+  nombre: "Rafael",
+  apellido: "Villarreal",
+  telefono: "0203343450",
+  ubicaciones: {
+    ciudad: "Venezuela",
+    direccion: "Avenidad principal 332",
+  },
+});
+agregarContactos({
+  id: 6,
+  nombre: "Jordan",
+  apellido: "Villarreal",
+  telefono: "05345450",
+  ubicaciones: {
+    ciudad: "Venezuela",
+    direccion: "Avenidad principal 322",
+  },
+});
+agregarContactos({
+  id: 7,
+  nombre: "jennifer",
+  apellido: "Villarreal",
+  telefono: "04114143450",
+  ubicaciones: {
+    ciudad: "Venezuela",
+    direccion: "Avenidad principal 252",
+  },
+});
 
 //Borrar contacto
 
-function quitarContactos(removerContacto) {
-  const posicion = listaContactos.indexOf(removerContacto);
-  if (posicion !== -1) {
-    listaContactos.splice(posicion, 1);
+function eliminarContacto(id) {
+  let indice = -1;
+  for (let i = 0; i < listaContactos.length; i++) {
+    if (id === listaContactos[i].id) {
+      indice = i;
+      break;
+    }
+  }
+
+  if (indice >= 0) {
+    let contactoEliminado = listaContactos.splice(indice, 1);
+    console.log(`Contacto con id ${id} eliminado`);
+    console.log("Contacto eliminado...", contactoEliminado);
+  } else {
+    console.log("El contacto no existe...");
   }
 }
-
-quitarContactos("Benjamin Reyes");
 
 //Imprimir Contacto
 
 function imprimirContactos() {
-  for (let i = 0; i < listaContactos.length; i++) {
-    console.log(listaContactos[i]);
-  }
+  console.log("lista de contactos...");
+  console.log(listaContactos);
 }
 
+imprimirContactos();
+
+eliminarContacto(1);
+
+console.log("Lista de Contactos Actualizada!");
 imprimirContactos();
